41621bdec0ae5728bfa32a1ab71516997a9eea3c
Daniele Bogo <me@bogodaniele.com>
2019-08-02 15:33:28 +0100

    Clean Fetching state

--- Flux/Stores/FetchingStatus.swift
+++ Flux/Stores/FetchingStatus.swift
@@ -1,24 +1,7 @@
 import Foundation
 
 enum FetchingStatus: Equatable {
-    case stationary
+    case idle
     case fetching
     case fetchingCompleted(error: ServiceError?)
-    
-    static func == (lhs: FetchingStatus, rhs: FetchingStatus) -> Bool {
-        switch (lhs, rhs) {
-        case (.stationary, .stationary):
-            return true
-        case (.fetching, .fetching):
-            return true
-        case (.fetchingCompleted, .fetchingCompleted):
-            return true
-        default:
-            return false
-        }
-    }
-
-    func isFetching() -> Bool {
-        return self == .fetching
-    }
 }
